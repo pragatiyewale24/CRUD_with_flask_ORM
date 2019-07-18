47981ee62fb4ebf9bfaacbd5749f27cd033225d6
pragatiyewale <pragati.yewale@monadinfotech.com>
2019-07-18 11:47:17 +0530

    first commit

--- admin.py
+++ admin.py
@@ -24,7 +24,7 @@ class Admin(db.Model):
     def __repr__(self):
         return 'success'
 
-class Stud24(db.Model):
+class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True)
     class_name=db.Column(db.String(20),nullable=False)
     is_monitor=db.Column(db.Integer,nullable=False)
@@ -37,7 +37,7 @@ class Stud24(db.Model):
 
     
 
-class Teacher24(db.Model):
+class Teacher(db.Model):
     teacher_id = db.Column(db.Integer, primary_key=True)
     department_name=db.Column(db.String(20),nullable=False)
     is_hod=db.Column(db.Integer,nullable=False)
@@ -48,7 +48,7 @@ class Teacher24(db.Model):
       self.is_hod=is_hod
       self.teaching_subject=teaching_subject
 
-class User24(db.Model):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     fname=db.Column(db.String(20),nullable=False)
     lname=db.Column(db.String(20),nullable=False)
@@ -66,14 +66,12 @@ class User24(db.Model):
 
 @app.route('/')
 def index():
-    # db.create_all()
     return render_template('login.html')  
 
 @app.route('/adminlogin', methods=['GET', 'POST'])
 def adminlogin():
     if request.method == "POST":
         try:
-            print('adminlogin')
             data = request.get_json()
             uname=data.get('username')
             pass1=data.get('password1')
@@ -81,11 +79,17 @@ def adminlogin():
             if registered_admin is not None:
                 response_data={
                     'status_code':200,
-                    'success':True,
+                    'message':True,
                     }
                 return jsonify(response_data)
+            else:
+                return jsonify({'result':'Incorrect Username or Password...' }) 
         except:
-            return jsonify({'result':'Incorrect Username or Password...' }) 
+             response_data={
+                    'status_code':501,
+                    'result':'Something went wrong....'
+                     }
+             return jsonify(response_data)
 
             # cur.execute('SELECT uname,pass FROM admin WHERE uname = %s AND pass = %s', (uname, pass1))
             # idpass = cur.fetchone()
@@ -128,34 +132,34 @@ def admininsert():
             return jsonify({'result':'Data added successfully!' })
 
     except:     
-        return jsonify({'result':'Something went wrong!' })
+        response_data={
+                    'status_code':501,
+                    'result':'Something went wrong....'
+                     }
+        return jsonify(response_data)
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
 
-@app.route('/student',methods=['POST'])
-def student():
-    if request.method=='POST':
-        response_data = {
-                "result": 'Switched to student',
-                "sucess": True,
-                "status_code": 200,             
-                }  
-        return jsonify(response_data)
+# @app.route('/student',methods=['POST'])
+# def student():
+#     if request.method=='POST':
+#         response_data = {
+#                 "result": 'Switched to student',
+#                 "sucess": True,
+#                 "status_code": 200,             
+#                 }  
+#         return jsonify(response_data)
 
 @app.route('/studentinfo')
 def studentinfo():
     # import pdb;pdb.set_trace()
-    # cur.execute("SELECT  * FROM stud24")
+    # cur.execute("SELECT  * FROM stud")
     # data = cur.fetchfirst()
-    # return render_template('studentdata.html', stud24=data)
-    data=Stud24.query.all()
-    print(data)
-    for item in data:
-        print(item)
-    return render_template('studentdata.html', stud24 = Stud24.query.all() )
+    # return render_template('studentdata.html', stud=data)
+    return render_template('studentdata.html', student = Student.query.all() )
 
 @app.route('/studentinsert', methods=['POST'])
 def studentinsert():
@@ -165,54 +169,63 @@ def studentinsert():
             data = request.get_json()
             class_name=data.get('class_name')
             studying_subjects=data.get('studying_subjects')
-            # import pdb; pdb.set_trace()
             if data.get('is_monitor')=="1":
                 is_monitor=1
             else:
                 is_monitor=0
-
-            entry_student=Stud24(class_name=class_name,studying_subjects=studying_subjects,is_monitor=is_monitor)
+            entry_student=Student(class_name=class_name,studying_subjects=studying_subjects,is_monitor=is_monitor)
             db.session.add(entry_student)
             db.session.commit()
-            # cur.execute("INSERT INTO stud24(class_name,is_monitor,studying_subjects) VALUES (  %s, %s, %s)", (class_name, is_monitor, studying_subjects))
+            # cur.execute("INSERT INTO stud(class_name,is_monitor,studying_subjects) VALUES (  %s, %s, %s)", (class_name, is_monitor, studying_subjects))
             # mysql.commit()                    
             return jsonify({'result':'Student data added successfully!' })
 
     except:     
-        return jsonify({'result':'Something went wrong!' })
+        response_data={
+        'status_code':501,
+        'result':"Something Went wrong..."
+        }
+        return jsonify(response_data)
 
 @app.route('/studentdelete/<string:id_data>', methods = ['GET'])
 def studentdelete(id_data):
-    # import pdb;pdb.set_trace()
-    # cur.execute("DELETE FROM stud24 WHERE id=%s", (id_data))
-    # mysql.commit()
-    delete_student=Stud24.query.filter_by(student_id=id_data).first()
-    db.session.delete(delete_student)
-    db.session.commit()
-    return redirect(url_for('studentinfo'))
+    try:
+        # import pdb;pdb.set_trace()
+        # cur.execute("DELETE FROM stud WHERE id=%s", (id_data))
+        # mysql.commit()
+        delete_student=Student.query.filter_by(student_id=id_data).first()
+        db.session.delete(delete_student)
+        db.session.commit()
+        return redirect(url_for('studentinfo'))
+    except:     
+        response_data={
+        'status_code':501,
+        'result':"Something Went wrong..."
+        }
+        return jsonify(response_data)
 
 
 @app.route('/studentupdate',methods=['POST','GET'])
 def studentupdate():
     try:
         if request.method == 'POST':
-            #import pdb; pdb.set_trace();
             data = request.get_json()
             id_data=data.get('id')
             class_name=data.get('class_name')
+            studying_subjects=data.get('studying_subjects')
+            is_monitor=0
             if data.get('is_monitor')=="1":
                 is_monitor=1
             else:
                 is_monitor=0
-
-            studying_subjects=data.get('studying_subjects')
-            update_student=Stud24.query.filter_by(student_id=id_data).first()
+            print(is_monitor)
+            update_student=Student.query.filter_by(student_id=id_data).first()
             update_student.class_name=class_name
             update_student.is_monitor=is_monitor
             update_student.studying_subjects=studying_subjects
             db.session.commit()
             # cur.execute("""
-            # UPDATE stud24
+            # UPDATE stud
             # SET class_name=%s, is_monitor=%s, studying_subjects=%s  
             # WHERE id=%s""", (class_name, is_monitor,studying_subjects, id_data))
             # mysql.commit()
@@ -224,25 +237,13 @@ def studentupdate():
 
 
 '''**********Teacher Module***********'''
-@app.route('/teacher',methods=['POST'])
-def teacher():
-    if request.method=='POST':
-        response_data = {
-                "result": 'Switched to teacher',
-                "sucess": True,
-                "status_code": 200,             
-                }  
-        return jsonify(response_data)
-
-
-
 @app.route('/teacherinfo')
 def teacherinfo():
     # import pdb;pdb.set_trace()
-    # cur.execute("SELECT  * FROM teacher24")
+    # cur.execute("SELECT  * FROM teacher")
     # data = cur.fetchfirst()
-    # return render_template('teacherdata.html', teacher24=data)
-    return render_template('teacherdata.html', teacher24 = Teacher24.query.all() )
+    # return render_template('teacherdata.html', teacher=data)
+    return render_template('teacherdata.html', teacher = Teacher.query.all() )
 
 @app.route('/teacherinsert', methods=['POST'])
 def teacherinsert():
@@ -257,24 +258,28 @@ def teacherinsert():
                 is_hod=0
             else:
                 is_hod=1
-            # cur.execute("INSERT INTO teacher24(department_name,is_hod,teaching_subject) VALUES (  %s, %s, %s)", (department_name, is_hod, teaching_subject))
+            # cur.execute("INSERT INTO teacher(department_name,is_hod,teaching_subject) VALUES (  %s, %s, %s)", (department_name, is_hod, teaching_subject))
             # mysql.commit()                    
-            entry_teacher=Teacher24(department_name=department_name,is_hod=is_hod,teaching_subject=teaching_subject)
+            entry_teacher=Teacher(department_name=department_name,is_hod=is_hod,teaching_subject=teaching_subject)
             db.session.add(entry_teacher)
             db.session.commit()
-            # cur.execute("INSERT INTO stud24(class_name,is_monitor,studying_subjects) VALUES (  %s, %s, %s)", (class_name, is_monitor, studying_subjects))
+            # cur.execute("INSERT INTO student(class_name,is_monitor,studying_subjects) VALUES (  %s, %s, %s)", (class_name, is_monitor, studying_subjects))
             # mysql.commit()                    
             return jsonify({'result':'Teacher data added successfully!' })
 
     except:     
-        return jsonify({'result':'Something went wrong!' })
+        response_data={
+        'status_code':501,
+        'result':'Something went wrong....'
+        }
+        return jsonify(response_data)
 
 @app.route('/teacherdelete/<string:id_data>', methods = ['GET'])
 def teacherdelete(id_data):
     # import pdb;pdb.set_trace()
-    # cur.execute("DELETE FROM teacher24 WHERE id=%s", (id_data))
+    # cur.execute("DELETE FROM teacher WHERE id=%s", (id_data))
     # mysql.commit()
-    delete_teacher=Teacher24.query.filter_by(teacher_id=id_data).first()
+    delete_teacher=Teacher.query.filter_by(teacher_id=id_data).first()
     db.session.delete(delete_teacher)
     db.session.commit()
     return redirect(url_for('teacherinfo'))
@@ -294,11 +299,11 @@ def teacherupdate():
                 is_hod=1
             teaching_subject=data.get('teaching_subject')
             # cur.execute("""
-            # UPDATE teacher24
+            # UPDATE teacher
             # SET department_name=%s, is_hod=%s, teaching_subject=%s  
             # WHERE id=%s""", (department_name, is_hod,teaching_subject, id_data))
             # mysql.commit()
-            update_teacher=Teacher24.query.filter_by(teacher_id=id_data).first()
+            update_teacher=Teacher.query.filter_by(teacher_id=id_data).first()
             update_teacher.department_name=department_name
             update_teacher.is_hod=is_hod
             update_teacher.teaching_subject=teaching_subject
@@ -322,12 +327,10 @@ def user():
 @app.route('/userinfo')
 def userinfo():
         # import pdb;pdb.set_trace()
-        # cur.execute("SELECT  * FROM user24")
+        # cur.execute("SELECT  * FROM user")
         # data = cur.fetchfirst()
-        # return render_template('userdata.html', user24=data)
-
-        
-        return render_template('userdata.html', user24 = User24.query.all() )
+        # return render_template('userdata.html', user=data)
+        return render_template('userdata.html', user= User.query.all() )
 
 @app.route('/userinsert', methods=['POST'])
 def userinsert():
@@ -341,32 +344,31 @@ def userinsert():
             email=data.get('email')
             mobileno1=data.get('phone')
             about=data.get('about')
-            registered_user = User24.query.filter_by(mobileno=mobileno1).first()
+            registered_user = User.query.filter_by(mobileno=mobileno1).first()
             if registered_user is not None:
                 return jsonify({'result':'Mobile number already exist.'}) 
             else:
-                import pdb; pdb.set_trace()
-                entry_user=User24(fname=fname,lname=lname,email=email,mobileno=mobileno1,about=about)
+                entry_user=User(fname=fname,lname=lname,email=email,mobileno=mobileno1,about=about)
                 db.session.add(entry_user)
                 db.session.commit()
                 return jsonify({'result':'User Data added successfully......' })
     except:
             return jsonify({'result':'Something went wrong..........' }) 
-            # cur.execute("SELECT mobileno FROM user24 WHERE mobileno=%s", (mobileno1))
+            # cur.execute("SELECT mobileno FROM user WHERE mobileno=%s", (mobileno1))
             # row=cur.fetchone()
             # if row is not None:            
             #     return jsonify({'result':'Mobile number already exist.'})
             # else:
-            #      cur.execute("INSERT INTO user24(fname,lname,email,mobileno,about) VALUES (  %s, %s, %s, %s, %s)", (fname, lname, email, mobileno1, about))
+            #      cur.execute("INSERT INTO user(fname,lname,email,mobileno,about) VALUES (  %s, %s, %s, %s, %s)", (fname, lname, email, mobileno1, about))
             #      mysql.commit()      
 
                  
 
 @app.route('/userdelete/<string:id_data>', methods = ['GET'])
 def userdelete(id_data):
-    # cur.execute("DELETE FROM user24 WHERE id=%s", (id_data))
+    # cur.execute("DELETE FROM user WHERE id=%s", (id_data))
     # mysql.commit()
-    delete_user=User24.query.filter_by(user_id=id_data).first()
+    delete_user=User.query.filter_by(user_id=id_data).first()
     db.session.delete(delete_user)
     db.session.commit()
     return redirect(url_for('userinfo'))
@@ -379,19 +381,17 @@ def userupdate():
         if request.method == 'POST':
             data = request.get_json()
             id_data=data.get('id')
-            print(id_data)
             fname=data.get('first_name')
-            print(fname)
             lname=data.get('last_name')
             email=data.get('email')
             mobileno1=data.get('phone')
             about=data.get('about')
             # cur.execute("""
-            # UPDATE user24
+            # UPDATE user
             # SET fname=%s, lname=%s, email=%s, mobileno=%s, about=%s 
             # WHERE id=%s""", (fname, lname, email, mobileno1, about,  id_data))
             # mysql.commit()
-            update_user=User24.query.filter_by(user_id=id_data).first()
+            update_user=User.query.filter_by(user_id=id_data).first()
             update_user.fname=fname
             update_user.lname=lname
             update_user.email=email
